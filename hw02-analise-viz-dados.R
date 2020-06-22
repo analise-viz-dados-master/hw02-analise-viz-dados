897b403a7f8f130895b25fb85a08a809344c5b17
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-06-21 21:23:00 -0300

    Adiciona solucao

--- hw02-analise-viz-dados.R
+++ hw02-analise-viz-dados.R
@@ -3,3 +3,10 @@ library(ggplot2); library(dplyr); library(readr); library(summarytools)
 df <- read_csv2("data-raw/notificacoes-covid19-mg.csv", col_types = c("nnnDccicncccccD"), locale = locale(encoding = "latin1", decimal_mark = ","))
 
 dfSummary(df)
+
+df %>% 
+  group_by(DATA_NOTIFICACAO, FAIXA_ETARIA, EVOLUCAO) %>% 
+  summarize(casos = n()) %>% 
+  ggplot() +
+  geom_line(aes(x = DATA_NOTIFICACAO, y = casos, color = EVOLUCAO)) +
+  facet_wrap(~ FAIXA_ETARIA)
